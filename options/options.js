1e5baf32c347d1c17a762c2046f5caf8d5005be8
Crowdbotics <team@crowdbotics.com>
2021-08-13 20:47:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lcapp_29793Identifier",
+  url: "https://lcapp_29793Identifier.botics.co",
+  api: "https://lcapp_29793Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
